02c689d1a789c170e64240172b39e7019494f894
Andrés Collares <andres.collares@fing.edu.uy>
2022-06-27 20:23:41 -0300

    partner

--- ciudadania/italiana/models.py
+++ ciudadania/italiana/models.py
@@ -1,13 +1,26 @@
-from neomodel import StructuredNode, StringProperty, DateProperty, BooleanProperty, RelationshipTo, IntegerProperty
+from neomodel import (
+    StructuredNode,
+    StructuredRel,
+    StringProperty,
+    DateProperty,
+    BooleanProperty,
+    RelationshipTo,
+    IntegerProperty,
+)
+
+
+class PartnerRel(StructuredRel):
+    is_married = BooleanProperty(required=True)
 
 
 class Person(StructuredNode):
     name = StringProperty(required=True)
-    family = IntegerProperty(required=True)
+    family = IntegerProperty(required=True, index=True)
     SEXES = {"male": "Male", "female": "Female", "other": "Other"}
     sex = StringProperty(required=True, choices=SEXES)
     birthday = DateProperty(required=False)
     has_citizenship = BooleanProperty(required=True)
     citizenship_resignation_date = DateProperty(required=False)
 
+    partner = RelationshipTo("Person", "PARTNER", model=PartnerRel)
     offspring = RelationshipTo("Person", "OFFSPRING")
