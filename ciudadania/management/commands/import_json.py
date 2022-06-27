8ede3bb4f26495a87e5d3520cfecd9dbed157c16
Andrés Collares <andres.collares@fing.edu.uy>
2022-06-27 20:46:52 -0300

    import partners

--- ciudadania/management/commands/import_json.py
+++ ciudadania/management/commands/import_json.py
@@ -18,34 +18,44 @@ class Command(BaseCommand):
             family_number = 0
             for family in data:
                 for person in family:
-                    create_person_descendants(person=person, family_number=family_number)
+                    create_person_and_descendants(person=person, family_number=family_number)
 
                 family_number += 1
 
             self.stdout.write(self.style.SUCCESS("Import successful"))
 
 
-def create_offspring_instances(parent, children):
-    for child in children:
-        create_person_descendants(person=child, parent=parent, family_number=parent.family)
+def create_person_and_descendants(person, family_number, parents=[]):
+    instance = create_person(person, family_number)
 
-def create_person_descendants(person, family_number, parent=None):
+    for parent in parents:
+        parent.offspring.connect(instance)
+
+    for partner in person["partners"]:
+
+        is_married = partner.pop("is_married", False)
+        partner_instance = create_person(partner, family_number=family_number)
+        instance.partner.connect(partner_instance, {"is_married": is_married})
+
+        children = partner["offspring"]
+        parents = [instance, partner_instance]
+        create_offspring_instances(parents=parents, family_number=family_number, children=children)
+
+
+def create_person(person, family_number):
     sex = person["sex"]
     name = names.get_full_name(gender=sex)
-    birthday = person["birthday"]
-    has_citizenship = person["has_citizenship"]
-    citizenship_resignation_date = person["citizenship_resignation_date"]
-    children = person["offspring"]
 
-    instance = Person.objects.create(
+    return Person.objects.create(
         name=name,
         family=family_number,
         sex=sex,
-        birthday=birthday,
-        has_citizenship=has_citizenship,
-        citizenship_resignation_date=citizenship_resignation_date,
+        birthday=person["birthday"],
+        has_citizenship=person["has_citizenship"],
+        citizenship_resignation_date=person["citizenship_resignation_date"],
     )
-    if parent:
-        parent.offspring.connect(instance)
 
-    create_offspring_instances(parent=instance, children=children)
+
+def create_offspring_instances(parents, family_number, children):
+    for child in children:
+        create_person_and_descendants(person=child, parents=parents, family_number=family_number)
