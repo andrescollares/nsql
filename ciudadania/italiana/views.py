a08a7309b0c8abf745f15aa6db540b0be71e151b
Agustín Bologna <agustin.bologna@gmail.com>
2022-07-03 17:15:18 -0300

    wip

--- ciudadania/italiana/views.py
+++ ciudadania/italiana/views.py
@@ -4,8 +4,9 @@ import uuid
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from datetime import datetime
+from datetime import datetime, date
 
+from neomodel import Q
 from neomodel.match import Traversal, OUTGOING, db
 
 from .models import Person, PartnerRel
@@ -25,13 +26,16 @@ def index(request):
         else:
             return HttpResponse("", status=404)
     else:
-        offspring = get_offspring("d1b44fb3-e415-458d-a8bf-edff17669cf9", "Jonathan Farley")
-        text= ""
-        for person in offspring:
-          if text=="":  
-            text = person.name
+        query = ('MATCH(p:Person) Return DISTINCT p.family_uuid')
+        results, meta = db.cypher_query(query)
+        family_uuids = [row[0] for row in results]
+        text = None
+        for item in family_uuids:
+          if not text:  
+            text = f"\"{item}\""
           else:
-            text += ", " + person.name
+            text += ", " + f"\"{item}\""
+        text = f"[{text}]"
         return HttpResponse(text)
 
 def add_family_uuid(member, family_uuid):
@@ -72,13 +76,31 @@ def get_offspring(family_uuid, name):
   offspring = [Person.inflate(row[0]) for row in results]
   return offspring
 
+def possible_citizenship(person, could_get_citizenship):
+    if(person.has_citizenship or could_get_citizenship.get(person.id)):
+        definition = dict(node_class=Person, direction=OUTGOING,relation_type="OFFSPRING", model=None)
+        relations_traversal = Traversal(person, Person.__label__,definition)
+        offspring = relations_traversal.all()
+        for member in offspring:
+            if not could_get_citizenship.get(member.id):
+                could_get_citizenship[member.id] = member
+            possible_citizenship(member, could_get_citizenship)
+    return could_get_citizenship
+
 @csrf_exempt
 def process_family(request, family_uuid):
     if request.method == "GET":
         family = Person.nodes.filter(family_uuid=family_uuid)
         # Dado que la ciudadanía se hereda desde familiares italianos
         # filtraremos por los mismos y haremos recorridas transversales
+        could_get_citizenship = dict()
         italian_relatives = family.filter(has_citizenship=True)
         for italian_relative in italian_relatives:
-            pass
-            # Hacer query que consiga a todos los descendientes de un italiano
+            could_get_citizenship = possible_citizenship(italian_relative, could_get_citizenship)
+        text= ""
+        for person in could_get_citizenship.values():
+          if text=="":  
+            text = person.name
+          else:
+            text += ", " + person.name
+        return HttpResponse(text)
